7e824e1bf50017cb0c04859713a46ed463fc56b2
Gabriel Panetta <ark.gp20@gmail.com>
2019-11-26 15:59:23 -0500

    Fix display sizing and make CE + DEL work

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 'use strict';
 
+let displayValue = "";
+const numbers = document.querySelectorAll('.numbers');
+const erase = document.querySelector('#erase');
+const deleteButton = document.querySelector('#delete');
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -35,3 +40,36 @@ function operate(num1, num2, operator) {
             break;                
     }
 }
+
+function display() {
+    const displayScreen = document.querySelector('#user-input div');
+    displayScreen.textContent = displayValue.replace(/(.)(?=(\d{3})+$)/g,'$1,');
+}
+
+numbers.forEach( (number) => {
+    number.addEventListener('click', function() {
+        if (displayValue.length < 15) {
+            displayValue += number.innerText;
+            display();
+        }
+        
+    });
+});
+
+erase.addEventListener('click', function() {
+    displayValue = "0";
+    display();
+    displayValue = "";
+});
+
+deleteButton.addEventListener('click', function() {
+    if (displayValue.length > 1) {
+        const substring = displayValue.substring(0, displayValue.length - 1);
+        displayValue = substring;
+        display();
+    } else {
+        displayValue = "0";
+        display();
+        displayValue = "";
+    }
+});
